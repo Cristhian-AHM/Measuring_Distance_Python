853c8b21252fbac479ba746d7ede1e235e9af6f5
Cristhian-AHM <14111367@itcj.edu.mx>
2019-02-10 18:02:31 -0700

    Se agregaron parametros para el tamaño maximo y mino del area

--- pyimagesearch/shapedetection.py
+++ pyimagesearch/shapedetection.py
@@ -6,7 +6,7 @@ class ShapeDetector:
 
 	# c es el contorno de la figura que intentamos reconocer.
 	# Para intentar hacer detección de figuras se usara aproximación por contornos.
-	def detect(self, c):	
+	def detect(self, c, maxArea, minArea):	
 		shape = "Sin identificar"
 		peri = cv2.arcLength(c, True)
 		area = cv2.contourArea(c)
@@ -19,7 +19,7 @@ class ShapeDetector:
 			shape = "Cuadrado" if ar >= 0.95 and ar <= 1.05 else "Rectangulo"
 		elif len(approx) == 5:
 			shape = "Pentagono"
-		if area > 210 and area < 1000 and shape == "Cuadrado" or shape == "Rectangulo" or shape == "Pentagono":
+		if area > minArea and area < maxArea and shape == "Cuadrado" or shape == "Rectangulo" or shape == "Pentagono":
 			status = True
 			color = (0, 255, 0)
 		else:
